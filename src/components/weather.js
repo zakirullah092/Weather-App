5b2cc51bccfafb35bbbdc9fbd5302019d71eb909
zakirullah092 <uzakir092@gmail.com>
2024-05-11 15:27:33 +0500

    first commit

---
+++ src/components/weather.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import  './weather.css' ;
+
+export default function App() {
+  const [city, setCity] = useState('');
+  const [error, setError] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const apiKey = 'd92c38b1e792b670a5cbdf0ed1c72231';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (city.trim() === '') {
+          return; // Exit early if city is empty
+        }
+
+        const response = await fetch(`${apiUrl}${city}`);
+        if (!response.ok) {
+          setError('Weather data not found for this location');
+          setWeather(null);
+          return;
+        }
+
+        const data = await response.json();
+        setWeather(data);
+        setError('');
+      } catch (error) {
+        setError('Error occurred while fetching data');
+        setWeather(null);
+      }
+    };
+
+    if (city.trim() !== '') {
+      fetchData(); // Call fetchData only if city is not empty
+    }
+  }, [city, apiUrl]); // useEffect dependency on 'city' and 'apiUrl'
+
+  const resetWeatherData = () => {
+    // Clear previous weather data and error message
+    setWeather(null);
+      setError('');
+      setCity('');
+  };
+
+  return (
+    <div className='main'>
+          <div>
+              
+          <input
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={resetWeatherData}>Clear</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Description: {weather.weather[0].description}</p>
+        </div>
+      )}
+     </div>
+    </div>
+  );
+}
